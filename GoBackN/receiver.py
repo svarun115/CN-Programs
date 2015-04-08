d847421b4cda6c5ff9f6813c4cadab49b5ede825
NihalHarish <nihal42harish@gmail.com>
2015-04-08 14:37:20 +0530

    Go Back N

--- GoBackN/receiver.py
+++ GoBackN/receiver.py
@@ -1,19 +1,29 @@
 import socket
+import signal
+import os
 
 client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_sock.connect((socket.gethostname(), 65535))
+client_sock.connect((socket.gethostname(), 60001))
 
-Rn = 0
+'''def handler(signo):
+   server_sock.close()
+   exit()
+signal.signal(signal.SIGINT, handler)'''
+Rn = 1
 #First Request
 client_sock.send(str(Rn).encode())
 
 while(True):
-   Sn = int(client_sock.recv(1024).decode())
+
+   Sn = int(client_sock.recv(1024).decode('utf8'))
+   print("Decoded", Sn)
    if(Sn == Rn):
       print("Accepted Packet No:", Sn)
       Rn += 1
-      client_sock.send(str(Rn).encode())
+      client_sock.send(str(Rn).encode('utf8'))
    else:
       #drop packet
       client_sock.send(str(Rn).encode())
-
+   if(Sn == 25):
+      break
+client_sock.close()
--- GoBackN/sender.py
+++ GoBackN/sender.py
@@ -1,21 +1,33 @@
 import socket
+import os
+import signal
 
 server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_sock.bind((socket.gethostname(), 65535))
+server_sock.bind((socket.gethostname(), 60001))
 server_sock.listen(1)
+
+'''def handler(signo):
+   server_sock.close()
+   exit()
+
+signal.signal(signal.SIGINT, handler)'''
 
 #N = int(input("Set Window Size")
 N = 5
 
 Sb = 0
 Sm = N-1
+
+client, client_add = server_sock.accept()
 while(True):
-   client, client_add = server_sock.accept()
-   #print("Successful COnnection")
-   decoded = (server_sock.recv(1024).decode())
+   print("Successful COnnection")
+   decoded = client.recv(1024).decode('utf8')
    print(decoded)
    Rn = int(decoded)
    if(Rn > Sb):
       Sm += (Rn - Sb)
       Sb = Rn
-   
+      client.send(str(Rn).encode('utf8'))
+   if(Sb > 25):
+      break
+server_sock.close()
